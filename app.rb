18e60e701e3d1c01e2fd70bd90eb3b2efdcd9f16
Pierre Trollé <ptrolle@octo.com>
2017-01-19 18:34:19 +0100

    try without trailing slash

--- app.rb
+++ app.rb
@@ -15,7 +15,7 @@ class RouteApp < Sinatra::Base
     "Proud of grid"
   end
 
-  get '/grid/:id/' do
+  get '/grid/:id/?' do
     if params[:id]
       id = params[:id]
 
@@ -38,57 +38,7 @@ class RouteApp < Sinatra::Base
     'FAUSSE REQUETE'
   end
 
-  get '/grid/:id' do
-    if params[:id]
-      id = params[:id]
-
-      # getFormattedGrid
-      contenu = file_manager.getGrid(id)
-      grille = JSON.load(contenu)
-      grid = file_manager.reformat_grid(grille)
-
-      #evolve
-      next_grid = grid.next
-
-      #save grid
-      grid_to_json = file_manager.format_grid(next_grid).to_json
-      file_manager.save(grid_to_json, id)
-
-      #return grid
-      headers 'Access-Control-Allow-Origin' => '*'
-      return grid_to_json
-    end
-    'FAUSSE REQUETE'
-  end
-
-  post('/create') do
-    length = params['length'].to_i
-    height = params['height'].to_i
-    id = 100
-
-    grid = Grid.new(length, height)
-
-    for x in 1..length
-      for y in 1..height
-        grid.add_cell(x, y, TableView::Plays::DEAD)
-      end
-    end
-
-    grid.add_cell(4, 5, TableView::Plays::ALIVE)
-    grid.add_cell(5, 4, TableView::Plays::ALIVE)
-    grid.add_cell(5, 5, TableView::Plays::ALIVE)
-    grid.add_cell(5, 6, TableView::Plays::ALIVE)
-    grid.add_cell(6, 5, TableView::Plays::ALIVE)
-
-    grid_to_json = file_manager.format_grid(grid).to_json
-
-    file_manager.save(grid_to_json, id)
-
-    headers 'Access-Control-Allow-Origin' => '*'
-    return grid_to_json
-  end
-
-  post('/create/') do
+  post('/create/?') do
     length = params['length'].to_i
     height = params['height'].to_i
     id = 100
