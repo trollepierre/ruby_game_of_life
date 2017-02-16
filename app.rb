b305031c65588992012f7b2600b2b0fbd0ca6f7c
Pierre Trollé <ptrolle@octo.com>
2017-02-16 09:00:03 +0100

    content type json

--- app.rb
+++ app.rb
@@ -53,29 +53,23 @@ class RouteApp < Sinatra::Base
   get '/grids/:id' do
     id = params[:id]
 
-    # getFormattedGrid
     contenu = file_manager.readFile(id)
-
     grid = file_manager.reformat_grid(contenu)
 
-    #evolve
     next_grid = grid.next
 
-    #save grid
     grid_to_json = file_manager.new_format_grid(next_grid)
     file_manager.save(grid_to_json, id)
 
-    #return grid
     headers 'Access-Control-Allow-Origin' => '*'
-    return grid_to_json
+    content_type :json
+    grid_to_json
   end
 
   get '/newCreate/:id/height/:height/width/:width' do
-#  envoi d'un [ { x: 1, y:1, state : "dead" },
     id = params[:id].to_i
     height = params[:height].to_i
     length = params[:width].to_i
-    id = 100
 
     grid = Grid.new(length, height)
 
@@ -91,6 +85,7 @@ class RouteApp < Sinatra::Base
     file_manager.save(grid_to_json, id)
 
     headers 'Access-Control-Allow-Origin' => '*'
+    content_type :json
     grid_to_json
   end
 
@@ -119,7 +114,7 @@ class RouteApp < Sinatra::Base
     file_manager.save(grid_to_json, id)
 
     headers 'Access-Control-Allow-Origin' => '*'
-    binding.pry
+    content_type :json
     grid_to_json
   end
 end
